dd1f23ae6b0b7e6da9852e5a5b1ecbec2dd044d1
Kim Seonmin <w.hand715@gmail.com>
2023-04-16 02:06:28 +0900

    ApplicationRunner 강의 JSON 데이터 자동 추가 기능

--- src/main/java/com/sparta/springbasic/SpringbasicApplication.java
+++ src/main/java/com/sparta/springbasic/SpringbasicApplication.java
@@ -1,8 +1,22 @@
 package com.sparta.springbasic;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.springbasic.dto.CourseRequestDto;
+import com.sparta.springbasic.entity.Course;
+import com.sparta.springbasic.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +26,18 @@ public class SpringbasicApplication {
         SpringApplication.run(SpringbasicApplication.class, args);
     }
 
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            File json = ResourceUtils.getFile("classpath:CourseData.json");
+
+            List<CourseRequestDto> list = new ObjectMapper().readValue(json, new TypeReference<>() {});
+            List<Course> courses = list.stream().map(Course::new).collect(Collectors.toCollection(ArrayList::new));
+
+            courseRepository.saveAll(courses);
+        };
+    }
 }
